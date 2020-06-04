0943afdd459d1e6700dec8d6a5fcff34035a0258
Jason <jason.rostar@yahoo.com>
2020-06-04 14:30:55 -0700

    made item list preview on shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import {Route, Switch} from 'react-router-dom'
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component'
 
 
 function App() {
@@ -16,7 +11,7 @@ function App() {
     <div>
     <Switch>
      <Route exact path='/' component={HomePage} />
-     <Route path='/hats' component={HatsPage} />
+     <Route path='/shop' component={ShopPage} />
      </Switch>
     </div>
     )
