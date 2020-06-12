5ca60354252dd95da48bb4f24e9e19684bc7384f
Jason <jason.rostar@yahoo.com>
2020-06-11 17:13:17 -0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
      return (
     <div>
-    <h1>hello</h1>
+  
     <Switch>
      <Route exact path='/' component={HomePage} />
      <Route path='/shop' component={ShopPage} />
